e687ee55903a6f3e34c2a7f71d1c09ba2176defc
spark380@naver.com <spark380@naver.com>
2022-08-03 12:57:08 +0900

    V1.0.0.1

---
+++ otvagent/src/otvagent/Main.java
@@ -0,0 +1,32 @@
+package otvagent;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 int iIndex = 0;
+	      
+	      while(true)
+	      {
+	         iIndex++;
+	         System.out.println("Hello otv " + iIndex);
+	         
+	         if(iIndex >= 10000)
+	         {
+	            iIndex = 0;
+	         }
+	         else
+	         {
+	            try
+	            {
+	               Thread.sleep(1000);
+	            }
+	            catch (Exception e) 
+	            {
+	               // TODO: handle exception
+	            }
+	         }
+	      }
+	}
+
+}
